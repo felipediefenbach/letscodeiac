4eb7c0e41ba2505908997c573021c9902e934f6e
FelipeDie <felipe@conexaoinfraestrutura.inf.br>
2022-12-03 23:47:46 -0300

    fix: kubernets - fix2

--- cloud-init/backend.sh
+++ cloud-init/backend.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 hostnamectl set-hostname node-1
 dnf -y install epel-release python3
+setenforce 0
--- cloud-init/frontend.sh
+++ cloud-init/frontend.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 hostnamectl set-hostname master-node
 dnf -y install epel-release python3
+setenforce 0
